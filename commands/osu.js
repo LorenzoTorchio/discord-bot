7cc3f8667caccb53d86cbd0fe1a22f110a6b87bf
LorenzoTorchio <lorenzotorchios@gmail.com>
2025-03-25 11:38:34 -0300

    rank command

--- commands/osu.js
+++ commands/osu.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
+const axios = require("axios");
 const path = "./user_data.json";
 
 module.exports = {
 	name: "osu",
-	description: "Link your Discord username to an osu! username",
+	description: "Link your Discord username to an osu! username (one-time only)",
 	async execute(message, args) {
-		if (!args.length) return message.reply("Please provide your osu! username.");
+		// Restrict to a specific channel
+		const allowedChannelId = "1348459858029973597";
+		if (message.channel.id !== allowedChannelId) {
+			return message.reply(`This command can only be used in <#${allowedChannelId}>.`);
+		}
 
+		if (!args.length) return message.reply("Please provide your osu! username.");
 		const username = args.join(" ");
 		const discordId = message.author.id;
 
@@ -17,10 +23,55 @@ module.exports = {
 			userData = JSON.parse(fs.readFileSync(path, "utf8"));
 		}
 
-		// Store the osu! username
-		userData[discordId] = username;
-		fs.writeFileSync(path, JSON.stringify(userData, null, 2));
+		// Prevent changing osu! username once set
+		if (userData[discordId]) {
+			return message.reply("You have already linked an osu! username and cannot change it.");
+		}
 
-		message.reply(`Your osu! username has been set to **${username}**.`);
+		// Get osu! API credentials
+		const clientId = process.env.OSU_CLIENT_ID;
+		const clientSecret = process.env.OSU_CLIENT_SECRET;
+
+		try {
+			// Get an OAuth token
+			const tokenResponse = await axios.post("https://osu.ppy.sh/oauth/token", {
+				client_id: clientId,
+				client_secret: clientSecret,
+				grant_type: "client_credentials",
+				scope: "public"
+			});
+
+			const token = tokenResponse.data.access_token;
+
+			// Fetch user data to verify the username exists
+			const response = await axios.get(`https://osu.ppy.sh/api/v2/users/${username}/osu`, {
+				headers: { Authorization: `Bearer ${token}` }
+			});
+
+			const osuUser = response.data;
+
+			// Store the verified osu! username
+			userData[discordId] = osuUser.username;
+			fs.writeFileSync(path, JSON.stringify(userData, null, 2));
+
+			// Assign a role
+			const roleId = "1348444710921961553";
+			const role = message.guild.roles.cache.get(roleId);
+			if (role) {
+				const member = message.guild.members.cache.get(discordId);
+				if (member) {
+					await member.roles.add(role);
+					message.reply(`Your osu! username has been linked as **${osuUser.username}** 🎉 You have also been given the **${role.name}** role!`);
+				} else {
+					message.reply(`Your osu! username has been linked as **${osuUser.username}**, but I couldn't assign your role. Please check my permissions.`);
+				}
+			} else {
+				message.reply("osu! username linked, but the specified role was not found.");
+			}
+
+		} catch (error) {
+			console.error(error);
+			message.reply("Invalid osu! username or an error occurred.");
+		}
 	}
 };
---
+++ commands/osurank.js
@@ -0,0 +1,73 @@
+
+const axios = require("axios");
+const rankRoles = require("../config/rank_roles.js");
+
+module.exports = {
+	name: "osurank",
+	description: "Assigns a role based on osu! rank",
+	async execute(message, args) {
+		const fs = require("fs");
+		const path = "./user_data.json";
+		const discordId = message.author.id;
+
+		// Check if the user has linked an osu! account
+		if (!fs.existsSync(path)) return message.reply("No osu! usernames are stored yet.");
+		const userData = JSON.parse(fs.readFileSync(path, "utf8"));
+		if (!userData[discordId]) return message.reply("You haven't linked an osu! username. Use `!osu <username>` first.");
+
+		const osuUsername = userData[discordId];
+
+		// Get osu! API credentials
+		const clientId = process.env.OSU_CLIENT_ID;
+		const clientSecret = process.env.OSU_CLIENT_SECRET;
+
+		try {
+			// Get an OAuth token
+			const tokenResponse = await axios.post("https://osu.ppy.sh/oauth/token", {
+				client_id: clientId,
+				client_secret: clientSecret,
+				grant_type: "client_credentials",
+				scope: "public"
+			});
+
+			const token = tokenResponse.data.access_token;
+
+			// Fetch user data from osu! API
+			const response = await axios.get(`https://osu.ppy.sh/api/v2/users/${osuUsername}/osu`, {
+				headers: { Authorization: `Bearer ${token}` }
+			});
+
+			const osuUser = response.data;
+			const globalRank = osuUser.statistics.global_rank || 999999; // If unranked, set a high number
+
+			// Find the appropriate role based on rank
+			let assignedRoleId = rankRoles.default; // Default role
+			for (const rankThreshold in rankRoles) {
+				if (globalRank <= rankThreshold) {
+					assignedRoleId = rankRoles[rankThreshold];
+					break;
+				}
+			}
+
+			// Assign the role
+			const member = message.guild.members.cache.get(discordId);
+			if (member) {
+				// Remove any previous rank roles
+				for (const roleId of Object.values(rankRoles)) {
+					if (member.roles.cache.has(roleId)) {
+						await member.roles.remove(roleId);
+					}
+				}
+
+				// Add the new rank role
+				await member.roles.add(assignedRoleId);
+				message.reply(`You have been assigned the role for rank **#${globalRank}**!`);
+			} else {
+				message.reply("I couldn't assign your role. Please check my permissions.");
+			}
+		} catch (error) {
+			console.error(error);
+			message.reply("Error fetching osu! data. Make sure your username is correct.");
+		}
+	}
+};
